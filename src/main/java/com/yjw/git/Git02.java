aea8095e19283f545165ae573ee81fd49e42be18
yjw <2495788244@qq.com>
2022-06-02 10:16:03 +0800

    master在git02添加了代码

--- src/main/java/com/yjw/git/Git02.java
+++ src/main/java/com/yjw/git/Git02.java
@@ -11,6 +11,7 @@ public class Git02 {
     public static void main(String[] args) {
         System.out.println("...........");
         System.out.println("test用户修改了代码");
+        System.out.println("master在git02添加了代码");
     }
 }
 
