64084c2dd78a888af0b995760a5e9a6bc21b6db3
yjw <2495788244@qq.com>
2022-06-02 10:07:21 +0800

    添加git02文件

---
+++ src/main/java/com/yjw/git/Git02.java
@@ -0,0 +1,15 @@
+package com.yjw.git;
+
+/**
+ * @author: yjw
+ * @create: 2022/6/2
+ * @FileName: Git02
+ * @History:
+ * @description:
+ */
+public class Git02 {
+    public static void main(String[] args) {
+        System.out.println("...........");
+    }
+}
+
