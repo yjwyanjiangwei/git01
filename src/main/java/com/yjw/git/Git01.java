f359aefbf51496ee3cdaa4c512fbf36a2381c428
yjw <2495788244@qq.com>
2022-06-02 09:24:13 +0800

    first update

---
+++ src/main/java/com/yjw/git/Git01.java
@@ -0,0 +1,15 @@
+package com.yjw.git;
+
+/**
+ * @author: yjw
+ * @create: 2022/6/2
+ * @FileName: Git01
+ * @History:
+ * @description:
+ */
+public class Git01 {
+    public static void main(String[] args) {
+        System.out.println("没哟具体的需求");
+    }
+}
+
